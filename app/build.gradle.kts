7f352249bc48c43f332e38e9325b7eb85a1d0f4e
merkost <konstantinlikes@gmail.com>
2023-04-23 09:35:03 +1000

    NumberRow implemented

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -51,9 +51,6 @@ android {
 
 dependencies {
 
-    implementation("com.google.accompanist:accompanist-pager-indicators:0.30.0")
-
-
     implementation("androidx.core:core-ktx:1.10.0")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.1")
     implementation("androidx.activity:activity-compose:1.7.1")
@@ -63,6 +60,7 @@ dependencies {
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.material3:material3")
+    implementation("androidx.compose.material:material-icons-extended")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
--- app/src/main/java/com/merkost/metronome/MainMetronomeScreen.kt
+++ app/src/main/java/com/merkost/metronome/MainMetronomeScreen.kt
@@ -6,19 +6,27 @@ import OutlinedCircle
 import androidx.compose.animation.core.SpringSpec
 import androidx.compose.animation.core.tween
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.Remove
 import androidx.compose.material.icons.filled.Settings
+import androidx.compose.material3.Card
 import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.LocalTextStyle
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Slider
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
@@ -26,9 +34,12 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.merkost.metronome.ui.theme.MetronomeTheme
@@ -38,6 +49,7 @@ val BallSize = 40.dp
 val CircleSize = BallSize + 32.dp
 val CircleWeight = 5.dp
 val BallColor = Color.LightGray
+
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -60,8 +72,8 @@ fun MainMetronomeScreen() {
         Column(
             modifier = Modifier
                 .fillMaxSize()
-                .padding(it)
-                .padding(top = 128.dp)
+                .padding(it),
+            horizontalAlignment = Alignment.CenterHorizontally
         ) {
 
             val springSpec = SpringSpec<Float>(
@@ -70,29 +82,51 @@ fun MainMetronomeScreen() {
                 dampingRatio = 0.8f
             )
 
-            val tweenSpec = tween<Float>(100)
-
             var selectedIndex by remember {
                 mutableStateOf(0)
             }
             val itemCount = 4
-            Row(
-                modifier = Modifier.fillMaxWidth(),
-                horizontalArrangement = Arrangement.Center
-            ) {
-                MetronomeBalls(
-                    selectedIndex = selectedIndex,
-                    itemCount = itemCount,
+            MetronomeBalls(
+                modifier = Modifier.padding(top = 128.dp, bottom = 64.dp),
+                selectedIndex = selectedIndex,
+                itemCount = itemCount,
 //                    arrangementSpacing = ,
-                    animSpec = springSpec,
-                    indicator = {
-                        OutlinedCircle()
-                    },
+                animSpec = springSpec,
+                indicator = {
+                    OutlinedCircle()
+                },
+            ) {
+                (1..itemCount).forEach {
+                    val color = if (it == 1) Color.Black else BallColor
+                    Ball(color = color)
+                }
+            }
+            Column(
+                modifier = Modifier.fillMaxWidth(),
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                Text(
+                    text = "Allegro",
+                    style = MaterialTheme.typography.bodyLarge,
+                    textAlign = TextAlign.Center
+                )
+                Row(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(horizontal = 32.dp),
+                    horizontalArrangement = Arrangement.SpaceBetween,
+                    verticalAlignment = Alignment.CenterVertically,
                 ) {
-                    (1..itemCount).forEach {
-                        val color = if (it == 1) Color.Black else BallColor
-                        Ball(color = color)
-                    }
+                    MyIconButton(modifier = Modifier.size(80.dp), Icons.Default.Remove) {}
+                    Text(
+                        modifier = Modifier.weight(1f),
+                        text = "168",
+                        style = MaterialTheme.typography.displayLarge.copy(
+                            fontWeight = FontWeight.ExtraBold,
+                            textAlign = TextAlign.Center
+                        )
+                    )
+                    MyIconButton(modifier = Modifier.size(80.dp), Icons.Default.Add) {}
                 }
             }
 
@@ -109,6 +143,16 @@ fun MainMetronomeScreen() {
                 }
             }
 
+        }
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun MyIconButton(modifier: Modifier, icon: ImageVector, onClick: () -> Unit) {
+    Card(modifier = modifier, onClick = onClick, shape = CircleShape) {
+        Box(modifier = Modifier.fillMaxSize()) {
+            Icon(icon, icon.name, modifier = Modifier.align(Alignment.Center))
         }
     }
 }
