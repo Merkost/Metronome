8c92c225087678551f5366866b4666995f597ed1
merkost <konstantinlikes@gmail.com>
2023-05-01 21:40:26 +1000

    Added Koin DI

---
+++ app/src/main/java/com/merkost/metronome/app/MetronomeApp.kt
@@ -0,0 +1,27 @@
+package com.merkost.metronome.app
+
+import android.app.Application
+import com.merkost.metronome.di.appModule
+import org.koin.android.ext.koin.androidContext
+import org.koin.android.ext.koin.androidLogger
+import org.koin.androidx.compose.BuildConfig
+import org.koin.core.context.startKoin
+import org.koin.core.logger.Level
+
+class MetronomeApp : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+
+        startKoin {
+            androidLogger(if (BuildConfig.DEBUG) Level.ERROR else Level.NONE)
+            androidContext(this@MetronomeApp)
+            modules(
+                listOf(
+                    appModule,
+                )
+            )
+        }
+
+    }
+}
