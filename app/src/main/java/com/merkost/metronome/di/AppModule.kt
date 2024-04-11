4378e7ea0bff47ac2efd27bf30b09f78e82fe414
merkost <konstantinlikes@gmail.com>
2024-04-11 15:27:26 +1000

    Moved SoundPool creation to Koin DI

--- app/src/main/java/com/merkost/metronome/di/AppModule.kt
+++ app/src/main/java/com/merkost/metronome/di/AppModule.kt
@@ -1,5 +1,8 @@
 package com.merkost.metronome.di
 
+import android.media.AudioAttributes
+import android.media.AudioManager
+import android.media.SoundPool
 import com.merkost.metronome.model.AppDatastore
 import com.merkost.metronome.model.AppDatastoreImpl
 import com.merkost.metronome.viewModels.MetronomeViewModel
@@ -13,5 +16,16 @@ val appModule = module {
     single<AppDatastore> { AppDatastoreImpl(androidContext()) }
 
     single { MetronomeViewModel(get()) }
+    factory {
+        SoundPool.Builder()
+            .setMaxStreams(4) // to prevent delaying the next tick under any circumstances
+            .setAudioAttributes(
+                AudioAttributes.Builder().setFlags(AudioAttributes.FLAG_AUDIBILITY_ENFORCED)
+                    .setLegacyStreamType(AudioManager.STREAM_NOTIFICATION)
+                    .setUsage(AudioAttributes.USAGE_ASSISTANT)
+                    .setContentType(AudioAttributes.CONTENT_TYPE_SONIFICATION)
+                    .build()
+            ).build()
+    }
     viewModel { SettingsViewModel(get()) }
 }
