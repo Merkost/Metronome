227150bca582fbbc292160487262333ef6d09576
merkost <konstantinlikes@gmail.com>
2024-08-23 12:27:03 +1000

    Version 1.0.3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -18,8 +18,8 @@ android {
         applicationId = "com.merkost.metronome"
         minSdk = 26
         targetSdk = 35
-        versionCode = 3
-        versionName = "1.0.2"
+        versionCode = 4
+        versionName = "1.0.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
